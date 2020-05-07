00d15f19797e876b3745ac9892e8c609d0b58c12
JingYi Liu <42438673+Jeansix@users.noreply.github.com>
2020-05-07 10:41:08 +0800

    submission of assignment-2 of 17307130236 (#46)
    * submission of assignment-1 of 17307130236
    
    * submission of assignment-2 of 17307130236
    
    * Update data.py
    
    * Update pt.py
    
    Co-authored-by: JingYi Liu <42438673+Jean-ljy@users.noreply.github.com>

--- assignment-2/example/handout/pt.py
+++ assignment-2/example/handout/pt.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import torch
 import torch.nn as nn
@@ -76,7 +75,8 @@ def evaluate(model):
     # for o in list(zip(datas[2], res))[:20]:
     #     print(o[0], o[1], o[0]==o[1])
 
-    print('accuracy is: %g' % np.mean([o[0]==o[1] for o in zip(datas[2], res)]))
+    print('accuracy is: %g' %
+          np.mean([o[0] == o[1] for o in zip(datas[2], res)]))
 
 
 def pt_main():
