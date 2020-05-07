95819f2ab346ff744f62a471cb7d475da0e01112
Cautiousss <48676630+Cautiousss@users.noreply.github.com>
2020-05-07 10:44:08 +0800

    submission of assignment-2 of 17307130334 (#52)
    * 17307130334
    
    * submission of assignment-1 of 17307130334
    
    * submission of assignment-2 of 17307130334
    
    * submission of assignment-2 of 17307130334

---
+++ assignment-2/17307130334/source.py
@@ -0,0 +1,21 @@
+
+import argparse
+from handout import *
+
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--framework', choices=['tf', 'pt'], default='pt')
+    arg = parser.parse_args()
+
+    if arg.framework == 'pt':
+        pt_main()
+        pt_adv_main()
+
+    elif arg.framework == 'tf':
+        tf_main()
+        tf_adv_main()
+    else:
+        raise RuntimeError
